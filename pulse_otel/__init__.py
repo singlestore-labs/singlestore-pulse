e1d516114f27ab53d7b94e7fb743d68a478bcc3d
Ashutosh Anshu <aanshu@singlestore.com>
2025-06-12 18:02:36 +0530

    Remove deprecated files and update pulse_agent to pulse_agent2; adjust version in setup.py

--- pulse_otel/__init__.py
+++ pulse_otel/__init__.py
@@ -1,1 +1,1 @@
-from pulse_otel.main import Pulse, CustomFileSpanExporter, FileLogExporter, pulse_agent, pulse_tool, s2_agent1
+from pulse_otel.main import Pulse, CustomFileSpanExporter, FileLogExporter, pulse_agent, pulse_tool, pulse_agent2
--- test_mod/__init__.py
+++
@@ -1,1 +0,0 @@
-from test_mod.main import s2_agent1
