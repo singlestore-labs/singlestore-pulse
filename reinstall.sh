83c60fa11207081004fb1fb6bb323c318e3b2ac3
Nuno Gonçalves <93784204+nunogoncalves03@users.noreply.github.com>
2026-01-12 12:34:17 +0000

    Bump version to 0.4.7 and update installation scripts (#57)

--- reinstall.sh
+++ reinstall.sh
@@ -3,5 +3,5 @@ set -e
 
 python3 setup.py sdist bdist_wheel
 pip uninstall singlestore_pulse -y
-pip install dist/singlestore_pulse-0.4.6-py3-none-any.whl
+pip install dist/singlestore_pulse-0.4.7-py3-none-any.whl
 # pytest -v --tb=short
